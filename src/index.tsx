f4226731b02940e5461da8306ef8500126895b4b
Emil Rosin <SaltisRS>
2026-05-15 03:21:26 +0200

    fix preflight issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import { serve } from "bun";
 import index from "./index.html";
-import { join } from "path";
 import { serveCompetitionTop5 } from "./embed/handlers";
-
-const API_URL = process.env.BUN_PUBLIC_API_URL ?? "http://localhost:8000";
-await Bun.write(
-  join(import.meta.dir, "dev-config.js"),
-  `// Written by dev server - do not edit manually\nwindow.__API_URL__ = window.__API_URL__ || ${JSON.stringify(API_URL)};\n`,
-);
 
 const server = serve({
   routes: {
